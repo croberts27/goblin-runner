4f1c40c7f1cba3c17d16a83ce37893842ddeea19
Calvin Roberts <you@example.com>
2024-02-04 19:46:05 -0700

    added font surface and text

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ screen = pygame.display.set_mode((600, 400))  # this is set on x,y-axis. x = 800
 pygame.display.set_caption("Goblin Runner")
 # create clock object to help control frame rate
 clock = pygame.time.Clock()
+# init font
+test_font = pygame.font.Font('fonts/zeropixel-eye-fs.ttf', 50)
+
+# init surfaces
 hills_surface = pygame.image.load('images/hills.JPG')
 ground_surface = pygame.image.load('images/ground.JPG')
+text_surface = test_font.render('Goblin Runner', False, 'black')
 
 while True:
     for event in pygame.event.get():
@@ -21,8 +26,9 @@ while True:
             exit()
     # fill screen with color to wipe anything away from last frame
     screen.fill("purple")
-    screen.blit(hills_surface, (0,0))
-    screen.blit(ground_surface, ((0,320)))
+    screen.blit(hills_surface, (0, 0))
+    screen.blit(ground_surface, (0, 320))
+    screen.blit(text_surface, (70, 25))
 
     # update everything to keep display open
     pygame.display.update()
