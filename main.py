63b6f7002849c4f26bd8078d7d4118669bdc63be
Calvin Roberts <you@example.com>
2024-02-04 19:22:45 -0700

    added surfaces

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 # import pygame
 import pygame
 from sys import exit
+
 # init pygame
 pygame.init()
 
 # create display surface
-screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((600, 400))  # this is set on x,y-axis. x = 800 y = 400. x, y read FROM left FROM top
 # update game title
 pygame.display.set_caption("Goblin Runner")
+# create clock object to help control frame rate
+clock = pygame.time.Clock()
+hills_surface = pygame.image.load('images/hills.JPG')
+ground_surface = pygame.image.load('images/ground.JPG')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all elements
+    # fill screen with color to wipe anything away from last frame
+    screen.fill("purple")
+    screen.blit(hills_surface, (0,0))
+    screen.blit(ground_surface, ((0,320)))
+
     # update everything to keep display open
     pygame.display.update()
+    # set frame rate ceiling
+    clock.tick(60)
