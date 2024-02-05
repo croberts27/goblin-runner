ac8b4be726080fca22bef7e048cab455cc50b6f3
Calvin Roberts <you@example.com>
2024-02-04 21:38:01 -0700

    getrect()

--- main.py
+++ main.py
@@ -5,23 +5,33 @@ from sys import exit
 # init pygame
 pygame.init()
 
-# create display surface
-screen = pygame.display.set_mode((600, 400))  # this is set on x,y-axis. x = 800 y = 400. x, y read FROM left FROM top
+# create display surf
+dis_width = 600
+dis_height = 400
+
+# this is set on x,y-axis. x = 800 y = 400. x, y read FROM left FROM top
+screen = pygame.display.set_mode((dis_width, dis_height))
+
 # update game title
 pygame.display.set_caption("Goblin Runner")
+
 # create clock object to help control frame rate
 clock = pygame.time.Clock()
 # init font
 goblin_font = pygame.font.Font('fonts/zeropixel-eye-fs.ttf', 50)
 
-# init surfaces
-hills_surface = pygame.image.load('images/hills.JPG')
-ground_surface = pygame.image.load('images/ground.JPG')
-text_surface = goblin_font.render('Goblin Runner', False, 'black')
-snail_surface = pygame.image.load('images/enemy/snail1.png')
+# init surfs
+hills_surf = pygame.image.load('images/hills.JPG').convert_alpha()
+ground_surf = pygame.image.load('images/ground.JPG').convert_alpha()
+text_surf = goblin_font.render('Goblin Runner', False, 'black')
+snail_surf = pygame.image.load('images/enemy/snail1.png').convert_alpha()
+player_surf = pygame.image.load('images/player/player_walk_1.png').convert_alpha()
 
-# init surface positions
-snail_x_pos = 600
+# rectangles
+player_rect = player_surf.get_rect(midbottom=(70, 345))
+snail_rect = snail_surf.get_rect(bottomright=(800, 345))
+
+# init surf positions
 snail_y_pos = 320
 
 while True:
@@ -31,14 +41,15 @@ while True:
             exit()
     # fill screen with color to wipe anything away from last frame
     screen.fill("purple")
-    screen.blit(hills_surface, (0, 0))
-    screen.blit(ground_surface, (0, 320))
-    screen.blit(text_surface, (70, 25))
-    snail_x_pos -= 3
-    if snail_x_pos < -100:
-        snail_x_pos = 600
+    screen.blit(hills_surf, (0, 0))
+    screen.blit(ground_surf, (0, 320))
+    screen.blit(text_surf, (70, 25))
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf, player_rect)
 
-    screen.blit(snail_surface,(snail_x_pos, snail_y_pos))
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left=800
 
     # update everything to keep display open
     pygame.display.update()
