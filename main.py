ce31dd6c1a5415d423f54f0e8f6be116e584ba7c
Calvin Roberts <you@example.com>
2024-02-08 13:08:42 -0700

    updated game over screen

--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 import pygame
 from sys import exit
 
+
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surf = goblin_font.render(f'Score: {current_time}', False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center=(300, 45))
+    screen.blit(score_surf, score_rect)
+
+
 # init pygame
 pygame.init()
 
 # create display surf
-dis_width = 600
-dis_height = 400
-
 # this is set on x,y-axis. x = 800 y = 400. x, y read FROM left FROM top
-screen = pygame.display.set_mode((dis_width, dis_height))
+screen = pygame.display.set_mode((600, 400))
 
 # update game title
 pygame.display.set_caption("Snail Runner")
@@ -20,23 +25,27 @@ clock = pygame.time.Clock()
 # init font
 goblin_font = pygame.font.Font('fonts/zeropixel-eye-fs.ttf', 50)
 
-game_active = True
-game_over_surf = goblin_font.render('GAME OVER!', False, 'Red')
-game_over_rect = game_over_surf.get_rect(center=(300, 100))
-restart_surf = goblin_font.render("'R' TO RESTART", False, 'Red')
-restart_rect = game_over_surf.get_rect(center=(240, 200))
+game_active = False
+start_time = 0
+game_over_surf = goblin_font.render('GAME OVER!', False, (210, 4, 45))
+game_over_rect = game_over_surf.get_rect(center=(300, 50))
+restart_surf = goblin_font.render("'R' TO RESTART", False, (210, 4, 45))
+restart_rect = game_over_surf.get_rect(center=(240, 150))
 
 # init surfs and rects
 hills_surf = pygame.image.load('images/hills.JPG').convert_alpha()
 ground_surf = pygame.image.load('images/ground.JPG').convert_alpha()
 
-score_surf = goblin_font.render('Snail Runner', False, (64, 64, 64))
-score_rect = score_surf.get_rect(center=(300, 45))
+# score_surf = goblin_font.render('Snail Runner', False, (64, 64, 64))
+# score_rect = score_surf.get_rect(center=(300, 45))
 
 player_surf = pygame.image.load('images/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(70, 345))
 player_gravity = 0
-player_pos = 0
+
+player_stand = pygame.image.load('images/player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rect = player_stand.get_rect(center=(280, 300))
 
 snail_surf = pygame.image.load('images/enemy/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright=(800, 345))
@@ -65,13 +74,14 @@ while True:
             if event.key == pygame.K_r:
                 game_active = True
                 snail_rect.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active:
         screen.fill("purple")
         screen.blit(hills_surf, (0, 0))
         screen.blit(ground_surf, (0, 320))
-        screen.blit(score_surf, score_rect)
         screen.blit(snail_surf, snail_rect)
+        display_score()
 
         snail_rect.x -= 4
         if snail_rect.right <= 0:
@@ -89,13 +99,12 @@ while True:
             game_active = False
 
     else:  # Game over state
-        screen.fill('Yellow')
+        screen.fill((94, 129, 162))
         screen.blit(game_over_surf, game_over_rect)
         screen.blit(restart_surf, restart_rect)
+        screen.blit(player_stand, player_stand_rect)
 
     # update everything to keep display open
     pygame.display.update()
     # set frame rate ceiling
     clock.tick(60)
-
-
