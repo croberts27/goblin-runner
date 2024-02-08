572be8fdb56694d4575d8b4bacb8c0c05268884d
Calvin Roberts <you@example.com>
2024-02-07 18:24:40 -0700

    added jump functionality

--- main.py
+++ main.py
@@ -13,68 +13,72 @@ dis_height = 400
 screen = pygame.display.set_mode((dis_width, dis_height))
 
 # update game title
-pygame.display.set_caption("Goblin Runner")
+pygame.display.set_caption("Snail Runner")
 
 # create clock object to help control frame rate
 clock = pygame.time.Clock()
 # init font
 goblin_font = pygame.font.Font('fonts/zeropixel-eye-fs.ttf', 50)
 
-# init surfs
+# init surfs and rects
 hills_surf = pygame.image.load('images/hills.JPG').convert_alpha()
 ground_surf = pygame.image.load('images/ground.JPG').convert_alpha()
 
-score_surf = goblin_font.render('Score: ', False, 'black')
-score_rect = score_surf.get_rect(center=(125, 25))
+score_surf = goblin_font.render('Snail Runner', False, (64, 64, 64))
+score_rect = score_surf.get_rect(center=(300, 45))
 
 player_surf = pygame.image.load('images/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(70, 345))
+player_gravity = 0
 
 snail_surf = pygame.image.load('images/enemy/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright=(800, 345))
 
 
-# classes
-
-class Player:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.frame = 0
-        self.health = 100
-        # isJump and jumpCount should be attributes of Player.
-        self.isJumping = False
-        self.isFalling = False
+# CLASSES
 
 
 # init surf positions
 snail_y_pos = 320
-
+# WHILE LOOP
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEMOTION:
-        #     print(event.pos)
+
+        if event.type == pygame.MOUSEBUTTONDOWN and player_rect.bottom >= 345:
+            if player_rect.collidepoint(event.pos):
+                player_gravity = -20
+
+        if event.type == pygame.KEYDOWN and player_rect.bottom >= 345:
+            if event.key == pygame.K_SPACE:
+                player_gravity = -20
+
     # fill screen with color to wipe anything away from last frame
     screen.fill("purple")
     screen.blit(hills_surf, (0, 0))
     screen.blit(ground_surf, (0, 320))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
     screen.blit(score_surf, score_rect)
-    screen.blit(player_surf, player_rect)
     screen.blit(snail_surf, snail_rect)
 
     snail_rect.x -= 4
     if snail_rect.right <= 0:
         snail_rect.right = 800
 
-    # if player_rect.colliderect(snail_rect):
-    #     exit()
+    # PLAYER
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 345:
+        player_rect.bottom = 345
+    screen.blit(player_surf, player_rect)
 
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
+    # GAME OVER
+    if snail_rect.colliderect(player_rect):
+        pygame.quit()
+        exit()
 
     # update everything to keep display open
     pygame.display.update()
